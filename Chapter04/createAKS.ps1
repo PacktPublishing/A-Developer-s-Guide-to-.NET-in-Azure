8febd675f3eb278b5a54173e4e66ae17e1744d0b
Tamir AlB <albalkt@mcmaster.ca>
2023-01-29 20:49:11 -0500

    updated region and kubernetes version

--- Chapter04/createAKS.ps1
+++ Chapter04/createAKS.ps1
@@ -4,7 +4,7 @@ Update-Module Az
 
 Connect-AzAccount -Subscription $subscriptionName
 
-$region = "canadaeast"
+$region = "westus"
 $resourceGroup = "aks-demoapp-rg"
 
 Get-AzAksVersion -Location $region |
@@ -13,4 +13,4 @@ New-AzAksCluster -ResourceGroupName $resourceGroup `
                     -Name aks-demoapp-cluster `
                     -NodeCount 2 `
                     -NodeVmSize Standard_DS3_v2 `
-                    -KubernetesVersion 1.26.1
+                    -KubernetesVersion 1.24.6
