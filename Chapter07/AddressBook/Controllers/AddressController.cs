d3f43a7a196349bca30b3cc0b61b2003d348e6dc
Anuraj <aparameswaran@socxo.com>
2023-08-08 21:45:49 +0530

    Implemented CRUD operations.

--- Chapter07/AddressBook/Controllers/AddressController.cs
+++ Chapter07/AddressBook/Controllers/AddressController.cs
@@ -25,23 +25,53 @@ public class AddressBookController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById()
+    public IActionResult GetById(int id)
     {
-        throw new NotImplementedException();
+        var address = _addressBookDbContext.Addresses.Find(id);
+        if (address == null)
+        {
+            return NotFound();
+        }
+        return Ok(address);
     }
+
     [HttpPost]
     public IActionResult Post(Address address)
     {
-        throw new NotImplementedException();
+        _addressBookDbContext.Addresses.Add(address);
+        _addressBookDbContext.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = address.Id }, address);
     }
+
     [HttpPut("{id}")]
     public IActionResult Put(int id, Address address)
     {
-        throw new NotImplementedException();
+        if (id != address.Id)
+        {
+            return BadRequest();
+        }
+        var existingAddress = _addressBookDbContext.Addresses.Find(id);
+        if (existingAddress == null)
+        {
+            return NotFound();
+        }
+        existingAddress.Name = address.Name;
+        existingAddress.Email = address.Email;
+        _addressBookDbContext.Update(existingAddress);
+        _addressBookDbContext.SaveChanges();
+        return NoContent();
     }
+
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
-        throw new NotImplementedException();
+        var book = _addressBookDbContext.Addresses.Find(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        _addressBookDbContext.Addresses.Remove(book);
+        _addressBookDbContext.SaveChanges();
+        return NoContent();
     }
 }
